536dbc468f4bc9bc6bb9202513b111e3b2709d5f
Nevold <nevold@tut.by>
2020-10-13 16:32:11 +0300

    feat: add brackets not use

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let tempVar='';
+ for (let i=0;i <bracketsConfig.length;i++){
+  tempVar+=bracketsConfig[i].join('')
+  }
+  let tempVarStr=str.split('').sort().join('');
+
+  return tempVarStr.includes(tempVar);
 }
