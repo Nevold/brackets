35b15ad5bbe8e2aed8b4b13eb6e9dad0a933c8a4
Nevold <nevold@tut.by>
2020-10-13 23:48:59 +0300

    feat: add other solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  let tempVar='';
- for (let i=0;i <bracketsConfig.length;i++){
-  tempVar+=bracketsConfig[i].join('')
-  }
-  let tempVarStr=str.split('').sort().join('');
-
-  return tempVarStr.includes(tempVar);
+  let arrFirstSide=[];
+  let arrSecondSide=[];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      arrFirstSide.push(bracketsConfig[i][0]);
+      arrSecondSide.push(bracketsConfig[i][1]);
+    }
+  let arrStr=str.split('');  
+  let count=-1;
+  for (let i = 0; i < arrStr.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if(arrStr[i] === arrFirstSide[j] && arrStr[i + 1] === arrSecondSide[j]){
+          arrStr.splice(i, 2);
+          i=count;
+        }
+      }
+    }
+    return arrStr.length == 0 ? true: false
 }
